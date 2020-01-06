77e86c525f21f2e6883e6032b2b59c99696d1301
furui <furui@itongkong.com>
2020-01-06 13:37:57 +0800

    for the first

---
+++ src/UdpClient.java
@@ -0,0 +1,30 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import com.alibaba.fastjson.JSONObject;
+
+
+
+public class UdpClient {
+
+	public static void main(String[] args) throws Exception {
+		DatagramSocket socket = new DatagramSocket();
+
+		String s = getJsonStr();
+		byte[] buf = s.getBytes();
+		// 将数据打包
+		DatagramPacket packet = new DatagramPacket(buf, buf.length, InetAddress.getByName("192.168.11.139"), 6070);
+		socket.send(packet);
+		socket.close();
+
+	}
+
+	public static String getJsonStr() {
+		Person p = new Person();
+		p.setName("张三");
+		p.setAge(18);
+		p.setGender("男");
+		return JSONObject.toJSONString(p);
+	}
+}
---
+++ src/UdpServer.java
@@ -0,0 +1,34 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+import com.alibaba.fastjson.JSONObject;
+
+
+public class UdpServer {
+
+	public static void main(String[] args) throws Exception {
+		// 定义一个接收端，并且指定了接收的端口号
+		DatagramSocket socket = new DatagramSocket(6070);
+
+		while (true) {
+			byte[] buf = new byte[1024];
+			// 解析数据包
+			DatagramPacket packet = new DatagramPacket(buf, buf.length);
+
+			socket.receive(packet);
+
+			String ip = packet.getAddress().getHostAddress();
+
+			buf = packet.getData();
+
+			String data = new String(buf, 0, packet.getLength());
+
+			System.out.println("收到 " + ip + " 发来的消息：" + data);
+
+			// Person p = JSON.parseObject(data, new TypeReference<Person>() {});
+			Person p = JSONObject.parseObject(data, Person.class);// JSON字符串转对象
+			System.out.println(p.toString());
+		}
+
+	}
+}
